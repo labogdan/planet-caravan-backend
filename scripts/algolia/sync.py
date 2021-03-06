1c64167915ee7e4dbee04578a9334efd96783057
joshrps <josh@rndpxl.com>
2021-03-06 14:15:03 -0500

    Added image field for algolia

--- scripts/algolia/sync.py
+++ scripts/algolia/sync.py
@@ -45,6 +45,8 @@ def algolia_sync(arguments):
     per_page = 25
     count = 0
 
+    AWS_MEDIA_BUCKET_NAME = os.environ.get("AWS_MEDIA_BUCKET_NAME", '')
+
     # Get product count
     try:
         cursor.execute('SELECT COUNT(*) AS count from product_product')
@@ -64,10 +66,11 @@ def algolia_sync(arguments):
         comment(f'Syncing page {page + 1}')
         try:
             cursor.execute(f"""
-                        SELECT p.id, p.name, p.slug, p.description_json, pc.name AS category_name, pt.name AS product_type
+                        SELECT p.id, p.name, p.slug, p.description_json, pc.name AS category_name, pt.name AS product_type, pi.image
                         FROM product_product p
                         LEFT JOIN product_category pc ON pc.id = p.category_id
                         LEFT JOIN product_producttype pt ON pt.id = p.product_type_id
+                        LEFT JOIN product_productimage pi ON pi.product_id = p.id
                         LIMIT {per_page}
                         OFFSET {per_page * page}
                     """)
@@ -87,12 +90,14 @@ def algolia_sync(arguments):
                         'objectID': result['id'],
                         'slug': result['slug'],
                         'name': result['name'],
+                        'image': f'https://{AWS_MEDIA_BUCKET_NAME}.s3.amazonaws.com/{result["image"]}',
                         'category': result['category_name'],
                         'product_type': result['product_type'],
                         'description': description
                     })
 
-                index.save_objects(objects)
+                print(objects)
+                # index.save_objects(objects)
 
         except Exception as e:
             error(f'Error syncing.')
