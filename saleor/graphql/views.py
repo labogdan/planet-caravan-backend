4ddd8c692c9e9f9d0fee365b9871dbee88e12f37
joshrps <josh@rndpxl.com>
2021-04-05 20:28:43 -0400

    Query cache set to 6 hours

--- saleor/graphql/views.py
+++ saleor/graphql/views.py
@@ -40,7 +40,7 @@ API_PATH = SimpleLazyObject(lambda: reverse("api"))
 unhandled_errors_logger = logging.getLogger("saleor.graphql.errors.unhandled")
 handled_errors_logger = logging.getLogger("saleor.graphql.errors.handled")
 
-QUERY_CACHE_TIME = 60 * 60
+QUERY_CACHE_TIME = 60 * 60 * 6  # 6 Hours
 
 def tracing_wrapper(execute, sql, params, many, context):
     conn: DatabaseWrapper = context["connection"]
